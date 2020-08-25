19c7cbf7fdfadbccbbaa047a412fca3741a57ee4
wangyongqi <wangyongqi@baidu.com>
2020-08-25 10:25:23 +0800

    element-ui 使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="table-container">
+    <el-table
+        :data="tableData">
+      <el-table-column
+        prop="date"
+        label="姓名"
+        width="180">
+        <template slot-scope="scope">
+            <el-input v-model="scope.row.name" v-on:change="nameChange(scope.$index, scope.row.name)">
+                
+            </el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="年龄"
+        width="180">
+        <template slot-scope="scope">
+            <el-input v-model="scope.row.age">
+                
+            </el-input>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+        <template slot-scope="scope">
+            <el-input v-model="scope.row.address">
+                
+            </el-input>
+        </template>
+      </el-table-column>
+    </el-table>
+    <button v-on:click="addNew">新增</button>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    /**
+     * {
+     *      name: '',
+     *      age: 12,
+     *      address:''
+     * }
+     */
+    return {
+        tableData: [
+            {}
+        ]
+    };
+  },
+  watch: {
+    "scope.row.age": function() {
+        console.log('change')
+    }
+  },
+  methods: {
+      addNew: function() {
+        this.tableData.push({});
+    },
+    nameChange: function(index, name) {
+        // 遍历tableData
+        for (let i = 0; i < this.tableData.length; i++) {
+            if (i === index) {
+                continue;
+            }
+            if (name === this.tableData[i].name) {
+                alert('名称相等,请修改....  ')
+            }
+        }
+    }
   }
 }
 </script>
@@ -55,4 +97,9 @@ li {
 a {
   color: #42b983;
 }
+.table-container {
+    width: 80%;
+    margin: 0 auto;
+}
+
 </style>
